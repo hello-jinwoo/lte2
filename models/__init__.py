6a8cf289da5cdad4e4163370405d3743d146a4a3
hello-jinwoo <jinwoo-kim@yonsei.ac.kr>
2023-01-24 17:49:04 +0900

    fix error

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .models import register, make
-from . import edsr, rdn, rcan, swinir, edsr_m, edsr_m_bcp
+from . import edsr, rdn, rcan, swinir, edsr_m, edsr_m_bcp, edsr_jw
 from . import mlp
 
 from . import misc, liif, lte, ltep
