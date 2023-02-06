18ca1d6018ae83a6a952fa4db09ed5b735f86c49
hello-jinwoo <jinwoo-kim@yonsei.ac.kr>
2023-02-06 08:23:16 +0000

    add new model

--- models/salwnet1.py
+++ models/salwnet1.py
@@ -13,8 +13,7 @@ from models import register
 
 
 """
-decnet1 -> decnet2
-  - apply head (3 -> D) module before upsampling
+ScaleAdaptiveLocalWindow network (SALWnet)
 """
 
 def default_conv(in_channels, out_channels, kernel_size, bias=True):
@@ -116,13 +115,13 @@ class EDSR(nn.Module):
         # define head module
         m_head = [conv(args.n_colors, n_feats, kernel_size)]
 
-        # # define body module
-        # m_body = [
-        #     ResBlock(
-        #         conv, n_feats, kernel_size, act=act, res_scale=args.res_scale
-        #     ) for _ in range(n_resblocks)
-        # ]
-        # m_body.append(conv(n_feats, n_feats, kernel_size))
+        # define body module
+        m_body = [
+            ResBlock(
+                conv, n_feats, kernel_size, act=act, res_scale=args.res_scale
+            ) for _ in range(n_resblocks)
+        ]
+        m_body.append(conv(n_feats, n_feats, kernel_size))
 
         self.head = nn.Sequential(*m_head)
         # self.body = nn.Sequential(*m_body)
@@ -134,13 +133,16 @@ class EDSR(nn.Module):
             # define tail module
             
             hid_dim = len(self.args.upsample_mode) * n_feats if type(self.args.upsample_mode) == list else n_feats
-            self.tail = nn.Sequential(nn.Conv2d(hid_dim, n_feats, 15, 1, 7),
+            self.tail = nn.Sequential(nn.Conv2d(hid_dim, n_feats, 3, 1, 1),
                                       nn.LeakyReLU(inplace=True),
-                                      nn.Conv2d(n_feats, n_feats, 15, 1, 7),
-                                      nn.LeakyReLU(inplace=True),
-                                      nn.Conv2d(n_feats, n_feats, 15, 1, 7),
-                                      nn.LeakyReLU(inplace=True), 
-                                      nn.Conv2d(n_feats, self.out_dim, 1))
+                                      nn.Conv2d(n_feats, n_feats, 1))
+
+            mhsa_encoder_layer = nn.TransformerEncoderLayer(d_model=n_feats, nhead=1, batch_first=True)
+            self.mhsa_after_tail = nn.TransformerEncoder(mhsa_encoder_layer, num_layers=4)
+
+            self.final = nn.Sequential(nn.Conv2d(n_feats, n_feats, 3, 1, 1),
+                                       nn.LeakyReLU(inplace=True),
+                                       nn.Conv2d(n_feats, n_feats, 1))
             
 
     def imresize(self, x, scale_factor=None, size=None):
@@ -185,13 +187,75 @@ class EDSR(nn.Module):
         # res = self.body(x)
         # res += x
 
-        _,_,h,w = x.size()
+        B,D,h,w = x.size()
 
         up_x = self.imresize(x=x,
                              scale_factor=scale_factor,
                              size=size)
+        _,_,H,W = up_x.size()
         # up_x = self.head(up_x)
-        x = self.tail(up_x)
+        x = self.tail(up_x) # (B, D, H, W)
+
+        
+
+        # uws: upscale_window_size
+        # lws: local_window_size (have to be multiple of uws)
+        if scale_factor != None:
+            uws = math.ceil(scale_factor)
+        else:
+            uws = math.ceil(H/h)
+        lws = self.args.local_window_size
+        if lws % uws != 0:
+            if lws % uws > uws - lws % uws:
+                lws = lws + (uws - lws % uws)
+            else:
+                lws = lws - (lws % uws)
+
+        # pad input to the multiple of lws (H` and W` are multiple of lws)
+        pad_size = [0, 0, 0, 0]
+        if H % lws != 0:
+            h_margin = lws * (H // lws + 1) - H
+            pad_size[3] = h_margin
+        if W % lws != 0:
+            h_margin = lws * (W // lws + 1) - W
+            pad_size[1] = h_margin
+        pad_size = tuple(pad_size)
+        padded_x = F.pad(input=x, pad=pad_size, mode='reflect') # (B, D, H`, W`)
+
+        # uws
+        uws_patches = padded_x.unfold(2, lws, lws).unfold(3, uws, uws) # (B, D, h_uws_patches, w_uws_patches, uws, uws) - (A)
+        unfold_shape = uws_patches.size() # (B, D, h_uws_patches, w_uws_patches, uws, uws)
+        _,_,h_uws_patches,w_uws_patches,_,_ = uws_patches.size()
+        uws_patches = uws_patches.reshape(B, D, -1, uws*uws) # (B, D, N_uws_patches, uws*uws) - (B)
+        uws_feat = uws_patches.permute(0, 2, 3, 1).reshape(-1, uws*uws, D) # (B * N_uws_patches, uws*uws, D) - (C)
+
+        lws_patches = padded_x.unfold(2, lws, lws).unfold(3, lws, lws) # (B, D, h_lws_patches, w_lws_patches, lws, lws)
+        lws_patches = torch.avg(lws_patches, dim=(-2, -1)) # (B, D, h_lws_patches, w_lws_patches)
+        lws_patches = F.interpolate(lws_patches, scale_factor=lws//uws, mode='nearest') # (B, D, h_uws_patches, w_uws_patches)
+        lws_patches = lws_patches.reshape(B, D, -1)[..., None] # (B, D, N_uws_patches, 1)
+        lws_feat = lws_patches.permute(0, 2, 3, 1).reshape(-1, 1, D) # (B * N_uws_patches, 1, D)
+
+        # global
+        global_patches = torch.avg(x, dim=(2, 3), keepdim=True) # (B, D, 1, 1)
+        global_patches = F.interpolate(global_patches, size=(h_uws_patches, w_uws_patches), mode='nearest') # (B, D, h_uws_patches, w_uws_patches)
+        global_patches = global_patches.reshape(B, D, -1)[..., None] # (B, D, N_uws_patches, 1)
+        global_feat = global_patches.permute(0, 2, 3, 1).reshape(-1, 1, D) # (B * N_uws_patches, 1, D)
+        
+        # MHSA
+        fusion_feat = torch.cat([uws_feat, lws_feat, global_feat], dim=3) # (B * N_uws_patches, uws*uws+2, D)
+        x_mhsa = self.mhsa_after_tail(fusion_feat)
+        x_mhsa = x_mhsa[:, :-2, :] # cut out last two feat (lws, global) # (B * N_uws_patches, uws*uws, D) - (C`)
+        x_mhsa = x_mhsa.reshape(B, -1, uws*uws, D).permute(0, 3, 1, 2) # (B, D, N_uws_patches, uws*uws) - (B`)
+        x_mhsa = x_mhsa.reshape(B, D, h_uws_patches, w_uws_patches, uws, uws) # (B, D, h_uws_patches, w_uws_patches, uws, uws) - (A`)
+        output_h = unfold_shape[2] * unfold_shape[4]
+        output_w = unfold_shape[3] * unfold_shape[5]
+        x_mhsa = x_mhsa.permute(0, 1, 4, 2, 5, 3, 6).contiguous()
+        x_mhsa = x_mhsa.view(B, D, output_h, output_w) # (B, D, H`, W`)
+        x = x_mhsa[:, :, :H, :W] # cut out the padded part # (B, D, H, W)
+
+        # final CNN (n_feats -> 3)
+        x = self.final(x)
+
         return x
 
     def load_state_dict(self, state_dict, strict=True):
@@ -214,9 +278,10 @@ class EDSR(nn.Module):
                                    .format(name))
 
 
-@register('decnet2-light')
+@register('salwnet1-light')
 def make_edsr_light(n_resblocks=16, n_feats=32, res_scale=1, scale=2, 
-                    no_upsampling=False, upsample_mode='bicubic',rgb_range=1):
+                    no_upsampling=False, upsample_mode='bicubic',rgb_range=1,
+                    local_window_size=24):
     args = Namespace()
     args.n_resblocks = n_resblocks
     args.n_feats = n_feats
@@ -228,12 +293,14 @@ def make_edsr_light(n_resblocks=16, n_feats=32, res_scale=1, scale=2,
 
     args.rgb_range = rgb_range
     args.n_colors = 3
+    args.local_window_size = local_window_size
     return EDSR(args)
 
 
-@register('decnet2-baseline')
+@register('salwnet1-baseline')
 def make_edsr_baseline(n_resblocks=16, n_feats=64, res_scale=1, scale=2, 
-                       no_upsampling=False, upsample_mode='bicubic',rgb_range=1):
+                       no_upsampling=False, upsample_mode='bicubic',rgb_range=1,
+                       local_window_size=24):
     args = Namespace()
     args.n_resblocks = n_resblocks
     args.n_feats = n_feats
@@ -245,12 +312,14 @@ def make_edsr_baseline(n_resblocks=16, n_feats=64, res_scale=1, scale=2,
 
     args.rgb_range = rgb_range
     args.n_colors = 3
+    args.local_window_size = local_window_size
     return EDSR(args)
 
 
-@register('decnet2')
+@register('salwnet1')
 def make_edsr(n_resblocks=32, n_feats=256, res_scale=0.1, scale=2, 
-              no_upsampling=False, upsample_mode='bicubic', rgb_range=1):
+              no_upsampling=False, upsample_mode='bicubic', rgb_range=1,
+              local_window_size=24):
     args = Namespace()
     args.n_resblocks = n_resblocks
     args.n_feats = n_feats
@@ -262,4 +331,5 @@ def make_edsr(n_resblocks=32, n_feats=256, res_scale=0.1, scale=2,
 
     args.rgb_range = rgb_range
     args.n_colors = 3
+    args.local_window_size = local_window_size
     return EDSR(args)
